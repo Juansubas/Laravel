25131dab7a98cdbd76bff7d4c2ea4c03ab5ec1a5
juansubas <juansubas@gmail.com>
2023-06-08 00:18:13 -0500

    Creating MVC APP

--- project/routes/web.php
+++ project/routes/web.php
@@ -13,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'index');
-
-
-Route::view('/about', 'about');
-
-Route::view('/contact', 'contact');
+Route::view('/', 'index')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/contact', 'contact')->name('contact');
 
