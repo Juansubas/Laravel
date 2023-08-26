8cedc21e9a980c379c69c80e3325cead11e7543b
juansubas <juansubas@gmail.com>
2023-08-26 03:03:47 -0500

    The operations like Get and post work at this momment

--- crud-laravel/app/Http/Controllers/TaskController.php
+++ crud-laravel/app/Http/Controllers/TaskController.php
@@ -3,32 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class TaskController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index() : View
     {
-        return view('index');
+        $tasks = Task::latest()->paginate(3);
+        return view('index',['tasks'=>$tasks]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create() : View
     {
-        //
+        return view('create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $request->validate([
+           'title' => 'required',
+            'description' => 'required'
+        ]);
+
+        Task::create($request->all());
+        return redirect()->route('tasks.index')->with('success', 'Nueva tarea creada exitosamente!');
     }
 
     /**
@@ -42,7 +51,7 @@ class TaskController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Task $task)
+    public function edit(Task $task) : View
     {
         //
     }
