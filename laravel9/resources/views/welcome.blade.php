6c9659d43e7ecbb3a43c09bfb7e47964f0f6e667
juansubas <juansubas@gmail.com>
2023-10-10 19:03:34 -0500

    Installation TailWindCss

--- laravel9/resources/views/welcome.blade.php
+++ laravel9/resources/views/welcome.blade.php
@@ -17,5 +17,7 @@
     meta-description="Home description"
     :sum="2 + 2"
 >
-    <h1>Home</h1>
+    <h1 class="text-3xl text-blue-500 font-bold underline">
+        Hello world!
+    </h1>
 </x-layouts.app>
---
+++ laravel9/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      "./resources/**/*.balde.php",
+      "./resources/**/*.js",
+      "./resources/**/*.vue"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
